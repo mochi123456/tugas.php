2db215064a89b41c36674a11f3b8e2287280bfd6
mochi123456 <ryzkymadan@gmail.com>
2024-11-13 19:54:18 +0700

    Add files via upload

---
+++ controllers/class_barang.php
@@ -0,0 +1,29 @@
+<?php
+        class Barang{
+        private $dbh;
+        public function __construct($dbh){
+            $this->dbh = $dbh;
+        }
+
+        public function dataBarang(){
+            $sql="SELECT * FROM Barang";
+            $rs = $this->dbh->query($sql);
+            return $rs;
+        }
+        
+       
+        public function dataProduk(){
+            $sql="SELECT * FROM Produk";
+            $rs = $this->dbh->query($sql);
+            return $rs;
+        }
+        
+        public function getAllJenis(){
+            $sql = "SELECT * FROM jenis_barang";
+            // fungsi query, eksekusi query dan ambil datanya
+            $rs = $this->dbh->query($sql); 
+            return $rs;
+        }
+        }
+
+?>
